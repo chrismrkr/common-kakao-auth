56d136a815dae128c445014c5949108e9540876a
깃허브네임 <>
2023-08-24 00:01:32 +0900

    init

---
+++ src/main/java/common/loginapiserver/LoginApiServerApplication.java
@@ -0,0 +1,13 @@
+package common.loginapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/common/loginapiserver/LoginApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package common.loginapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
